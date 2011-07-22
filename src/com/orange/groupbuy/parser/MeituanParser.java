a7c57400549dfa73b00c3d150641584f958c0e60
unknown <dong@.(none)>
2011-07-22 14:51:50 +0800

    update meituanParser

--- src/com/orange/groupbuy/parser/MeituanParser.java
+++ src/com/orange/groupbuy/parser/MeituanParser.java
@@ -2,6 +2,7 @@ package com.orange.groupbuy.parser;
 
 import java.util.Date;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.jdom.Element;
@@ -14,10 +15,9 @@ import com.orange.groupbuy.dao.Product;
 import com.orange.groupbuy.manager.ProductManager;
 
 public class MeituanParser extends CommonGroupBuyParser {
-	final static String WEBSITE = "√¿Õ≈";
 	@Override
 	public boolean parseElement(Element root, CommonAddressParser addressParser) {
-		List<?> productList = getFieldBlock(root, "deals", "deal");
+		List<?> productList = getFieldBlock(root, "deals", "data");
 		if (productList == null)
 			return false;
 
@@ -26,37 +26,49 @@ public class MeituanParser extends CommonGroupBuyParser {
 			Element productElement = (Element) it.next();
 			if (productElement == null)
 				continue;
-			Element data = productElement;
-			String loc = getFieldValue(data, "deal_url");
-			String title = getFieldValue(data, "title");
-			String website = WEBSITE;
-			String siteurl = "";
-			String image = getFieldValue(data, "medium_image_url");
-			String city = getFieldValue(data, "division_name");
-			String startTimeString = getFieldValue(data, "start_date");
-			String endTimeString = getFieldValue(data, "end_date");
-			double value = StringUtil.doubleFromString(getFieldValue(data,
-					"value"));
-			double price = StringUtil.doubleFromString(getFieldValue(data,
-					"price"));
-			String description = getFieldValue(data, "description");
-			int bought = StringUtil.intFromString(getFieldValue(data,
-					"quantity_sold"));
-			//List<String> details = (List<String>) getFieldBlock(data, "detail");
-			//String detail = details.get(0);
-			String detail = "";
+			Element deal = getFieldElement(productElement, "deal");
+			String website = getFieldValue(deal, "website");
+			String city = getFieldValue(deal, "city_name");
+			String siteurl = getFieldValue(deal, "city_url");
+			String title = getFieldValue(deal, "deal_title");
+			String loc = getFieldValue(deal, "deal_url");
+			String image = getFieldValue(deal, "deal_img");
+			int category = convertCategory(getFieldValue(deal, "deal_cate"));
+			String description = getFieldValue(deal, "deal_desc");
+			double value = StringUtil.doubleFromString(getFieldValue(deal, "value"));
+			double price = StringUtil.doubleFromString(getFieldValue(deal, "price"));
+			int bought = StringUtil.intFromString(getFieldValue(deal, "sales_num"));
+			String startTimeString = getFieldValue(deal, "start_time");
+			String endTimeString = getFieldValue(deal, "end_time");
 			int major = DBConstants.C_NOT_MAJOR;
+			String detail = getFieldValue(deal, "deal_tips");
 
-			Date startDate = StringUtil.dateFromString(startTimeString);
-			Date endDate = StringUtil.dateFromString(endTimeString);
+			Date startDate = StringUtil.dateFromIntString(startTimeString);
+			Date endDate = StringUtil.dateFromIntString(endTimeString);
+			
+			Element shops = getFieldElement(productElement, "shops");
+			List<?> shopsList = getFieldBlock(shops, "shop");
+			List<String> addressList = new LinkedList<String>();
+			List<String> phoneList = new LinkedList<String>();
+			Iterator<?> shop_it = shopsList.iterator();
+			while (shop_it.hasNext()) {
+				Element addressElement = (Element) shop_it.next();
+				if (productElement == null)
+					continue;
+				String phone = getFieldValue(addressElement, "shop_tel");
+				String address   = getFieldValue(addressElement, "shop_addr");
+				phoneList.add(phone);
+				addressList.add(address);
+			}
 
 			Product product = saveProduct(mongoClient, city, loc, image, title, startDate, endDate, 
-					price, value, bought, siteId, website, siteurl, major, null, addressParser);
+					price, value, bought, siteId, website, siteurl, major, addressList, addressParser);
 			
 			if (product != null){							
 				// save extra fields
 				product.setDescription(description);
 				product.setDetail(detail);
+				product.setTel(phoneList);
 				product.setRange(null);
 				ProductManager.save(mongoClient, product);
 			}		
