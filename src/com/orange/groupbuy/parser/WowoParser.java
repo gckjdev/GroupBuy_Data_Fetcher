de5f7998c8a12e69f7ed7d2a73b6d9bc9acd645d
unknown <dong@.(none)>
2011-07-23 14:01:15 +0800

    add WowoParser

---
+++ src/com/orange/groupbuy/parser/WowoParser.java
@@ -0,0 +1,14 @@
+package com.orange.groupbuy.parser;
+
+import me.prettyprint.cassandra.service.template.SuperCfResult;
+
+public class WowoParser extends Hao123Parser {
+	
+	@Override
+	public String getEncoding() {
+		// TODO Auto-generated method stub
+		return "GBK";
+	}
+	
+
+}
