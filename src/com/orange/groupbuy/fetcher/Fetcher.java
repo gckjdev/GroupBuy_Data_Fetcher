1f4482c853f4f2188fb8d9a22873577daf7fd819
Benson Peng <penglingzhe@gmail.com>
2011-08-04 22:05:10 +0800

    add scheduler for fire the timer deep in night

--- src/com/orange/groupbuy/fetcher/Fetcher.java
+++ src/com/orange/groupbuy/fetcher/Fetcher.java
@@ -1,5 +1,8 @@
 package com.orange.groupbuy.fetcher;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 import java.util.Timer;
 
 import com.mongodb.DBObject;
@@ -48,6 +51,50 @@ public class Fetcher extends CommonProcessor {
         }
     }
 	
+	static class ResetTaskTimer extends java.util.TimerTask{
+		
+		Fetcher dataFetcher;
+		
+		public ResetTaskTimer(Fetcher dataFetcher){
+			this.dataFetcher = dataFetcher;
+		}
+		
+        @Override
+        public void run() {
+        	
+        	log.info("<ResetTaskTimer> timer fired");
+
+        	// fetch active task and put into queue
+    		FetchTaskManager.resetAllTask(dataFetcher.getMongoDBClient());
+    		
+    		// set next timer
+			Timer newResetTaskTimer = new Timer();
+			newResetTaskTimer.schedule(new ResetTaskTimer(dataFetcher), ResetTaskTimer.getTaskDate());
+        }
+        
+        static public Date getTaskDate(){
+        	
+        	int scheduleHour = 3;		// 3 AM of the day
+        	
+    		TimeZone timeZone = TimeZone.getTimeZone("GMT+0800");
+    		Calendar now = Calendar.getInstance(timeZone);
+    		now.setTime(new Date());
+    		
+    		if (now.get(Calendar.HOUR_OF_DAY) >= scheduleHour){
+    			now.add(Calendar.DAY_OF_MONTH, 1);
+    		}
+    		
+    		now.set(Calendar.HOUR_OF_DAY, scheduleHour);
+    		now.set(Calendar.MINUTE, 0);
+    		now.set(Calendar.SECOND, 0);
+    		now.set(Calendar.MILLISECOND, 0);    			
+    		
+        	log.info("<ResetTaskTimer> next timer set to "+now.getTime().toString());
+    		
+    		return now.getTime();
+        }
+    }
+	
 	public static void main(String[] args){		
 				
 		final int MAX_THREAD_NUM = 5;
@@ -60,8 +107,15 @@ public class Fetcher extends CommonProcessor {
 			if (i == 0){
 				Timer readTaskTimer = new Timer();
 				readTaskTimer.schedule(new ReadTaskTimerTask(dataFetcher), 1000, 1000);
+
+				Timer resetTaskTimer = new Timer();
+				resetTaskTimer.schedule(new ResetTaskTimer(dataFetcher), ResetTaskTimer.getTaskDate());
+				
+				// the following code is just for test
+				// resetTaskTimer.schedule(new ResetTaskTimer(dataFetcher), new Date());
 			}
 		}
+		
 	}
 
 }
