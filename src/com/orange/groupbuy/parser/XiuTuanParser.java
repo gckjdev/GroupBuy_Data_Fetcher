1df26cb4e8bbb43a032a7db7ab1c017056ab895f
kira.o <ouyongyong@163.com>
2011-09-09 14:35:25 +0800

    modify loc method

--- src/com/orange/groupbuy/parser/XiuTuanParser.java
+++ src/com/orange/groupbuy/parser/XiuTuanParser.java
@@ -7,8 +7,12 @@ public class XiuTuanParser extends Tuan800Parser {
 	@Override
 	public String getLoc(Element productElement){		
 		String loc =  getFieldValue(productElement, "loc");
-		int index = loc.indexOf((int)'?');
-		loc = loc.substring(0, index);		
+		if(loc == null)
+			return null;
+		int index = loc.indexOf("?");
+		if(index>0){
+			loc = loc.substring(0, index);
+		}
 		return loc;
 	}
 
