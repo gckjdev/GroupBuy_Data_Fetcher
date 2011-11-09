dd1ed39da8e0ffc380865852991f668dbacc464c
Benson Peng <penglingzhe@gmail.com>
2011-11-09 14:30:58 +0800

    remove WAP URL implementation on dianping & jumei

--- src/com/orange/groupbuy/parser/DianpingParser.java
+++ src/com/orange/groupbuy/parser/DianpingParser.java
@@ -2,15 +2,18 @@ package com.orange.groupbuy.parser;
 
 public class DianpingParser extends Hao123Parser {
 
-	@Override
-	public String generateWapLoc(String loc,String imageURL) {
+	// not work now
+	
+//	@Override
+//	public String generateWapLoc(String loc,String imageURL) {
+
 		
-		final String prefixWapURL = "http://m.t.dianping.com/deal/";
-		final String suffixWapURL = ""; //"?cityid=*&agent=*&version=*&screen=*&token=*&tag=deal";
-		String id = getIDFromWeb("deal/", null, loc);
-		if(id == null)
-			return null;
-		else 
-			return prefixWapURL.concat(id).concat(suffixWapURL);
-	}
+//		final String prefixWapURL = "http://m.t.dianping.com/deal/";
+//		final String suffixWapURL = ""; //"?cityid=*&agent=*&version=*&screen=*&token=*&tag=deal";
+//		String id = getIDFromWeb("deal/", null, loc);
+//		if(id == null)
+//			return null;
+//		else 
+//			return prefixWapURL.concat(id).concat(suffixWapURL);
+//	}
 }
--- src/com/orange/groupbuy/parser/JuMeiParser.java
+++ src/com/orange/groupbuy/parser/JuMeiParser.java
@@ -9,19 +9,19 @@ public class JuMeiParser extends Hao123Parser {
 		return true;
 	}
 	
-	@Override
-	public String generateWapLoc(String webURL, String imageURL){
-		
-//		wap：http://m.jumei.com/i/MobileHome/brief_view?hash_id=skinzjy0808
-//		web：http://www.jumei.com/i/deal/skinzjy0808.html
-		
-		final String basicWapURL = "http://m.jumei.com/i/MobileHome/brief_view?hash_id=";
-		String id = getIDFromWeb("deal/", ".html", webURL);
-		if(id == null || id.trim().isEmpty())
-			return null;
-		
-		String wapURL = basicWapURL.concat(id);
-		return wapURL;
-	}
+//	@Override
+//	public String generateWapLoc(String webURL, String imageURL){
+//		
+////		wap：http://m.jumei.com/i/MobileHome/brief_view?hash_id=skinzjy0808
+////		web：http://www.jumei.com/i/deal/skinzjy0808.html
+//		
+//		final String basicWapURL = "http://m.jumei.com/i/MobileHome/brief_view?hash_id=";
+//		String id = getIDFromWeb("deal/", ".html", webURL);
+//		if(id == null || id.trim().isEmpty())
+//			return null;
+//		
+//		String wapURL = basicWapURL.concat(id);
+//		return wapURL;
+//	}
 
 }
